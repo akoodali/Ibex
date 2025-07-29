ced9e39962086327582d5b0f90edc1130e0e7379
akoodali <arjunkoodalinair@gmail.com>
2025-07-28 17:41:18 -0700

    Added optree=0.12.0 to basiliskEnv.R

--- R/basiliskEnv.R
+++ R/basiliskEnv.R
@@ -11,7 +11,8 @@
           "keras=3.6.*",
           "tensorflow=2.18.*",
           "h5py=3.13",
-          "numpy=1.26"
+          "numpy=1.26",
+          "optree=0.12.0"
         )
       )
     }
